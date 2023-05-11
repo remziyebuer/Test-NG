70374c5e117caa94acf0d9903f4e907900974b03
isminiz <115974069+remziyebuer@users.noreply.github.com>
2023-05-11 23:57:24 +0200

    ...

---
+++ src/test/java/techproed/utilities/ListenersRetry.java
@@ -0,0 +1,19 @@
+package techproed.utilities;
+
+public class ListenersRetry {
+    /*
+   Bu sınıf sadece FAIL olan test case'leri tekrar çalıştırır
+   maxRetryCount ek olarak çalisma sayısıdır. Bu örnekte Fail olan test (maxRetryCount = 1) normal bir kere
+   çalıştıktan sonra fail olursa 1 kez daha çalışacaktır.
+    */
+    private int retryCount = 0;
+    private static final int maxRetryCount = 2;
+    @Override
+    public boolean retry(ITestResult result) {
+        if (retryCount < maxRetryCount) {
+            retryCount++;
+            return true;
+        }
+        return false;
+    }
+}
